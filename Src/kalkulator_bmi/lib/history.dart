2517d3f5680615efdf4daefe99e04ef0e0a76e7d
PratamaAngga <anggashillnoir@gmail.com>
2026-03-10 15:19:21 +0700

    Menambahkan menampilkan histori

--- Src/kalkulator_bmi/lib/history.dart
+++ Src/kalkulator_bmi/lib/history.dart
@@ -9,3 +9,19 @@ void simpanRiwayat(double berat, double tinggi, double bmi, String kategori) {
     'kategori': kategori,
   });
 }
+
+void tampilkanRiwayat() {
+  print('\n=== Riwayat Perhitungan ===');
+  if (riwayat.isEmpty) {
+    print('Belum ada data riwayat.');
+    return;
+  }
+
+  // for-in loop
+  for (var data in riwayat) {
+    print('Berat: ${data['berat']} kg | '
+        'Tinggi: ${data['tinggi']} cm | '
+        'BMI: ${(data['bmi'] as double).toStringAsFixed(2)} | '
+        'Status: ${data['kategori']}');
+  }
+}
