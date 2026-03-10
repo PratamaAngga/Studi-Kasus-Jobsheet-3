dc1abf0bee5a04dd7b9d37a48cc84e1420ec5ac0
PratamaAngga <anggashillnoir@gmail.com>
2026-03-10 13:09:42 +0700

    pembuatan project untuk studi kasus 1

---
+++ Src/tabel_perkalian/bin/tabel_perkalian.dart
@@ -0,0 +1,5 @@
+import 'package:tabel_perkalian/tabel_perkalian.dart' as tabel_perkalian;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tabel_perkalian.calculate()}!');
+}
---
+++ Src/tabel_perkalian/test/tabel_perkalian_test.dart
@@ -0,0 +1,8 @@
+import 'package:tabel_perkalian/tabel_perkalian.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
