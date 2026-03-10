f4d70dd016ffb5b62685d7e69e06f055204669ee
PratamaAngga <anggashillnoir@gmail.com>
2026-03-10 14:56:25 +0700

    Menambahkan function menentukan kategori bmi

--- Src/kalkulator_bmi/lib/bmi_calculator.dart
+++ Src/kalkulator_bmi/lib/bmi_calculator.dart
@@ -2,3 +2,15 @@ double hitungBMI(double beratKg, double tinggiCm) {
   final double tinggiM = tinggiCm / 100;
   return beratKg / (tinggiM * tinggiM);
 }
+
+String menentukanKategori(double bmi) {
+  if (bmi < 18.5) {
+    return 'Kurus';
+  } else if (bmi < 25) {
+    return 'Normal';
+  } else if (bmi < 30) {
+    return 'Gemuk';
+  } else {
+    return 'Obesitas';
+  }
+}
